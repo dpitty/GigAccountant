e48b625a4401443e2231bdbd197922c2bff70a0d
dpitty <dpitt00@gmail.com>
2020-05-11 22:49:42 -0700

    working prototype for taskrabbit, paypal, wyzant

--- gig_accountant.py
+++ gig_accountant.py
@@ -1,9 +1,10 @@
 import csv
 import locale #currency formatting
 from datetime import datetime
-
-print("Hello, gig accountant")
-
+'''
+This first protoype is built in May 2020
+Note that format of CSV records may change from PayPal, TaskRabbit
+'''
 class PaymentReceived:
 	#_publicAttr = 'whatever'
 
@@ -34,10 +35,64 @@ class TaskRabbitPayment(PaymentReceived):
 		result.__fullDict = fullDict
 		return result
 
+class PayPalPayment(PaymentReceived):
+
+	#return None for 'not a payment received'
+	@classmethod
+	def fromDict(cls, fullDict):
+		if not cls.isPayment(fullDict):
+			return None
+		result = cls(\
+			dollarAmount=float(fullDict['Amount']),\
+			date=datetime.strptime(fullDict['Date'], '%m/%d/%Y'),\
+			fromWho=fullDict['Name'],\
+			payMethod='PayPal')
+		#TODO use a constant variable for date format '%m/%d/%Y'?
+		result.__fullDict = fullDict
+		return result
+
+	@classmethod
+	def isPayment(cls, fullDict):
+		#Note, use == for string comparison. 'is' compares object id (not contents)
+		#Discard all blank Amounts and negatives Amounts
+		if fullDict['Amount'] == '' or float(fullDict['Amount']) <= 0:
+			return False
+		#Include only Status = Completed (ignore Pending, Cancelled, Reversed, Paid)
+		#YES Type = General Payment and Status = Completed
+		if fullDict['Type'] == 'General Payment' and fullDict['Status'] == 'Completed':
+			return True
+		#YES Type = Mobile Payment and Status = Completed
+		if fullDict['Type'] == 'Mobile Payment' and fullDict['Status'] == 'Completed':
+			return True
+		#YES Type = Website Payment and Status = Completed
+		if fullDict['Type'] == 'Website Payment' and fullDict['Status'] == 'Completed':
+			return True
+		#Anything else is not a payment, for now
+		#E.g.: Status = Paid and Type = Invoice Sent does not describe a payment...
+		return False
+
+
+class WyzantPayment(PaymentReceived):
+	#Note. No easy way to get a spreadsheet of payments... but maybe straight from HTML?
+	#Maybe a script that fills the form specifying dates and retrieves the data?
+	#I'm copy-pasting into a spreadsheet. It introducs all sorts of irregularities
+	#Like a ' ' at the end of column titles
+	@classmethod
+	def fromDict(cls, fullDict):
+		#Filter out Status incomplete? How often does that happen?
+		result = cls(\
+			dollarAmount=float(fullDict['Earned ']),\
+			date=datetime.strptime(fullDict['Date '].split(' ')[0], '%m/%d/%Y'),\
+			fromWho=fullDict['Student '].split(' \n')[0],\
+			payMethod='Wyzant')
+		#TODO use a constant variable for date format '%m/%d/%Y'?
+		result.__fullDict = fullDict
+		return result		
+
 
 def load_tr_payments(csvfilename):
 	result = []
-	with open(csvfilename) as csvfile:
+	with open(csvfilename, encoding='utf-8') as csvfile:
 		#discard first three lines
 		next(csvfile)
 		next(csvfile)
@@ -48,12 +103,51 @@ def load_tr_payments(csvfilename):
 			result.append(TaskRabbitPayment.fromDict(row_as_dict))
 	return result
 
+def load_pp_payments(csvfilename):
+	result = []
+	num_non_payment_rows = 0
+	#Need utf-8-sig to handle non-English symbols as well as BOM
+	#\ufeff
+	#https://stackoverflow.com/questions/17912307/u-ufeff-in-python-string
+	with open(csvfilename, encoding='utf-8-sig') as csvfile:
+		csvreader = csv.DictReader(csvfile)
+		for row_as_dict in csvreader:
+			p = PayPalPayment.fromDict(row_as_dict)
+			if p:
+				result.append(p)
+			else:
+				num_non_payment_rows += 1
+	print("Found",num_non_payment_rows,"Non-payment rows in",csvfilename)
+	return result
 
-locale.setlocale( locale.LC_ALL, '' ) #to print dollars format
-pr = PaymentReceived(fromWho='sanity test')
-print(pr)
+def load_wa_payments(csvfilename):
+	result = []
+	with open(csvfilename, encoding='utf-8') as csvfile:
+		csvreader = csv.DictReader(csvfile)
+		for row_as_dict in csvreader:
+			result.append(WyzantPayment.fromDict(row_as_dict))
+	return result
 
-tr_csvfilename = 'C:\\Users\\user\\Desktop\\taskrabbit2019.csv'
-tr_payments = load_tr_payments(tr_csvfilename) #list of PaymentReceived objects
-for p in tr_payments:
-	print(p)
+def main():
+	locale.setlocale( locale.LC_ALL, '' ) #to print dollars format
+	
+	#Test TaskRabbit
+	tr_csvfilename = 'C:\\Users\\user\\Desktop\\taskrabbit2019.csv'
+	tr_payments = load_tr_payments(tr_csvfilename) #list of PaymentReceived objects
+	for p in tr_payments:
+		print(p)
+
+	#Test PayPal
+	pp_csvfilename = 'C:\\Users\\user\\Desktop\\PayPal2019.csv'
+	pp_payments = load_pp_payments(pp_csvfilename)
+	for p in pp_payments:
+		print(p)
+
+	#Test Wzyant
+	wa_csvfilename = 'C:\\Users\\user\\Desktop\\wyzant2019detail.csv'
+	wa_payments = load_wa_payments(wa_csvfilename)
+	for p in wa_payments:
+		print(p)
+
+if __name__ == "__main__":
+    main()
